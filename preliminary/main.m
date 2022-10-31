0c71e4d20bbc1863769f79438d021fbb34113721
Jonah <jonahega@gmail.com>
2022-10-31 18:11:41 +0900

    Add errorbar

--- preliminary/main.m
+++ preliminary/main.m
@@ -12,10 +12,13 @@ for i = 1:n
     post = data(2, :);
     pre_scores(i) = sum(pre);
     post_scores(i) = sum(post);
+
 end
 
 pre_mean = mean(pre_scores);
 post_mean = mean(post_scores);
+pre_std = std(pre_scores);
+post_std = std(post_scores);
 
 x = categorical({'Pre-test', 'Post-test'});
 x = reordercats(x, {'Pre-test', 'Post-test'});
@@ -27,9 +30,9 @@ post_xs = ones(n) + 1;
 
 blue = [0 0.4470 0.7410];
 cyan = [0.3010 0.7450 0.9330];
-light_gray = [.7 .7 .7];
+light_gray = [.8 .8 .8];
 mid_gray = [.5 .5 .5];
-dark_gray = [.2 .2 .2];
+dark_gray = [.3 .3 .3];
 dark = [.1 .1 .1];
 
 %%%%%%%%%%%%%%
@@ -43,6 +46,9 @@ for i = 1:n
     hold on
     plot([1, 2], [pre_scores(i), post_scores(i)], Color=mid_gray, LineWidth=1)
 end
+hold on
+errorbar([1, 2], y, [pre_std, post_std], ...
+    'LineStyle','none', 'LineWidth', 1, 'Color', dark, 'CapSize', 8)
 hold off
 b.FaceColor = 'flat';
 b.CData(1,:) = light_gray;
@@ -59,6 +65,3 @@ ylim([0, 30])
 
 h = gca;
 set(h, 'FontSize', 14)
-
-%TODO: Add error bar
-
